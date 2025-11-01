f933174a2c8c05c5cbdddd09281414003b197aee
PTT154 <nguyenminhphuoc154@gmail.com>
2025-11-01 22:21:40 +0700

    luu va xuat cartList tu local

--- src/models/CartItemPhone.js
+++ src/models/CartItemPhone.js
@@ -1,8 +1,8 @@
 class CartItem {
-    constructor(id, name, price, img, color, storage, quantity, type) {
+    constructor(id, name, basePrice, img, color, storage, quantity, type) {
         this.id = id;
         this.name = name;
-        this.price = price;
+        this.basePrice = basePrice;
         this.img = img;
         this.color = color;
         this.storage = storage;
